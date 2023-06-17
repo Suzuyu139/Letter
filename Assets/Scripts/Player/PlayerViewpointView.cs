a07c7c42c651dc444ecd521a8a71a88121a518da
Suzuyu <akurai4107@gmail.com>
2023-06-17 15:07:06 +0900

    カメラ

--- Assets/Scripts/Player/PlayerViewpointView.cs
+++ Assets/Scripts/Player/PlayerViewpointView.cs
@@ -7,10 +7,16 @@ public class PlayerViewpointView : MonoBehaviour
     [SerializeField] Transform _camera = null;
 
     Transform _charaTransform = null;
+    Camera _mainCamera = null;
 
     private void Awake()
     {
+        _mainCamera = Camera.main;
         _charaTransform = this.transform;
+
+        _mainCamera.transform.SetParent(_camera);
+        _mainCamera.transform.localPosition = Vector3.zero;
+        _mainCamera.transform.localRotation = Quaternion.identity;
     }
 
     public void UpdateRotation(Quaternion cameraRot, Quaternion charaRot)
