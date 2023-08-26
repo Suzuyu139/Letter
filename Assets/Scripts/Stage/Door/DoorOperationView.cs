d69286d1aec0ad760cd48afbe583189121f52e75
Suzuyu <akurai4107@gmail.com>
2023-08-26 23:09:25 +0900

    ドア処理追加（途中）

---
+++ Assets/Scripts/Stage/Door/DoorOperationView.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using UniRx.Triggers;
+
+public class DoorOperationView : MonoBehaviour
+{
+    [SerializeField] DoorView _doorView;
+    [SerializeField] Collider[] _colliderAutoDoor = new Collider[] { };
+    [Space(10)]
+    [SerializeField] DoorType _doorType;
+
+    public void Initialize()
+    {
+        switch (_doorType)
+        {
+            case DoorType.Auto:
+                if (_colliderAutoDoor.Length <= 0)
+                {
+                    return;
+                }
+                for (int i = 0; i < _colliderAutoDoor.Length; i++)
+                {
+                    _colliderAutoDoor[i].OnTriggerEnterAsObservable()
+                        .Subscribe(OnAutoDoorTriggerEnter).AddTo(this);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    void OnAutoDoorTriggerEnter(Collider collider)
+    {
+        _doorView.Open();
+    }
+}
+
+public enum DoorType
+{
+    Auto,
+}
--- Assets/Scripts/Stage/Door/DoorView.cs
+++ Assets/Scripts/Stage/Door/DoorView.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DoorView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform _rightDoorTransform;
+    [SerializeField] Transform _leftDoorTransform;
+    [SerializeField] float _speed;
+    [SerializeField] float _movePosX;
+
+    public bool IsInitialized { get; private set; } = false;
+    public bool IsOpen { get; private set; } = false;
+
+    float _initRightDoorPosX = 0.0f;
+    float _initLeftDoorPosX = 0.0f;
+
+    public void Initialize()
     {
-        
+        if(IsInitialized)
+        {
+            return;
+        }
+
+        _initRightDoorPosX = _rightDoorTransform.localPosition.x;
+        _initLeftDoorPosX = _leftDoorTransform.localPosition.x;
+
+        IsInitialized = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Open(bool isAutoClose = false)
     {
-        
+        if (!IsInitialized)
+        {
+            Initialize();
+        }
+        if (IsOpen)
+        {
+            return;
+        }
+
+        IsOpen = true;
+        _rightDoorTransform.DOLocalMoveX(_movePosX, _speed);
+        _leftDoorTransform.DOLocalMoveX(-_movePosX, _speed);
+
+        if (isAutoClose)
+        {
+            DOVirtual.DelayedCall(5.0f, () =>
+            {
+                Close();
+            });
+        }
+    }
+
+    public void Close()
+    {
+        if (!IsInitialized)
+        {
+            Initialize();
+        }
+        if (!IsOpen)
+        {
+            return;
+        }
+
+        IsOpen = false;
+        _rightDoorTransform.DOLocalMoveX(_initRightDoorPosX, _speed);
+        _leftDoorTransform.DOLocalMoveX(-_initLeftDoorPosX, _speed);
     }
 }
